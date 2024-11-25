d954530ffaa857280e45f12743be0d204a12b47a
KNB5 <martin.basulto.n@gmail.com>
2024-11-25 13:17:23 -0300

    se quitaron bibliotecas que no se estaban utilizando

--- core/src/main/java/io/github/FirstGame/testGame/Nave4.java
+++ core/src/main/java/io/github/FirstGame/testGame/Nave4.java
@@ -2,14 +2,12 @@ package io.github.FirstGame.testGame;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.TimeUtils;
 
 public class Nave4 extends SpaceObject implements Shootable{
 
